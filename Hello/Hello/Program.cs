cab53d0dfbd2fcebbdaba24d46e7f20d58dfccb4
mahimanni <mannimahima@gmail.com>
2024-07-25 18:46:49 +0530

    Hello program

---
+++ Hello/Hello/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //adding some comment
+
+            //int var = 11;
+            //string r = Console.ReadLine();
+            //Console.WriteLine(r);
+            //Console.WriteLine("Hello World "+var+12);
+            //Console.WriteLine("Hello Planet");
+            //Console.Write("Now leaving the cursor on the same line using Write...");
+            //Console.WriteLine("Hello Earth");
+
+            //Data Types example
+            /*
+            int a = 34;
+            float b = 34.4F;
+            double c = 34.5;
+            Console.WriteLine(a +" "+b+" "+c);
+
+            bool flag = false;
+            Console.WriteLine(flag);
+
+
+            char ch = 'm';
+            Console.WriteLine(ch);
+
+            string str = "";
+            Console.WriteLine(str);
+            */
+
+
+            //Type Casting
+            /*int x = (int)4.8;
+            Console.WriteLine(x);
+
+            double y1 = (double)4.8;
+            Console.WriteLine(y1);
+
+            int x = 6;
+            double y = x;
+            Console.WriteLine(x+" "+y);
+            int z = 'a';
+            Console.WriteLine(z);*/
+
+            //Taking User input
+            //Console.WriteLine("Enter your name");
+            //string name=Console.ReadLine();
+            //Console.WriteLine("Hello, "+name);
+
+            //Console.WriteLine("How old are you?");
+            //string age = Console.ReadLine();
+
+            //Console.WriteLine("After 4 years you would be " + (Convert.ToInt32(age) + 4));
+
+            Console.WriteLine(Math.Sqrt(38));
+            Console.ReadLine();
+        }
+    }
+}
