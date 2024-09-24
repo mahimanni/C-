e3a049ca2cf30e2b4498d856ac1aba97e62f56fe
mahimanni <mannimahima@gmail.com>
2024-09-24 14:41:35 +0530

    Practical Programs of Test 1

---
+++ PracticalPrograms/Practical722003/Practical722003/Program.cs
@@ -0,0 +1,111 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Runtime.InteropServices;
+
+class AlmostPalindrome
+{
+    string? rev;
+    int flag = 0,i;
+    public AlmostPalindrome()
+    {
+        Console.WriteLine("Welcome to the program");
+        rev = "";
+    }
+    public string? reverse(String s)
+    {
+        rev = "";
+        for (i = s.Length - 1; i >= 0; i--)
+        {
+            rev += s[i];
+        }
+        return rev;
+    }
+    public bool almostPalin(String s)
+    {
+        int count = 0;
+        //Method-2
+        /*int j=s.Length-1;
+        for (i = 0; i <= s.Length / 2; i++)
+        {
+            if (s[i] != s[j]) count++;
+            j--;
+        }
+        if (count == 1) return true;
+        return false;*/
+
+        rev = reverse(s);
+        for (i = 0; i < s.Length; i++)
+        {
+            if (s[i] != rev[i])
+                count++;
+        }
+
+        if (count == 2)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public string? replaceLetter(String s)
+    {
+        string? r = "";
+        //Method-2
+        /*s = s.ToLower();
+        foreach (char item in s)
+        {
+            r += item - 'a' + 1;
+        }
+        return r;*/
+
+        int ind;
+        String bigalpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String smallalpha = "abcdefghijklmnopqrstuvwxyz";
+
+        for (i = 0; i < s.Length; i++)
+        {
+            ind = bigalpha.IndexOf(s[i]);
+            if (ind == -1)
+            {
+                ind = smallalpha.IndexOf(s[i]);
+                if (ind != -1)
+                {
+                    r += (ind + 1).ToString();
+                }
+                else
+                {
+                    flag = 1;
+                    break;
+                }
+            }
+            else
+            {
+                r += (ind + 1).ToString();
+            }
+        }
+        if (flag == 1) return "String is not only of alphabetic characters";
+        return r;
+    }
+}
+class Program
+{
+    public static void Main(string[] args)
+    {
+        AlmostPalindrome obj = new AlmostPalindrome();
+
+        Console.WriteLine("Enter a string");
+        string? s = Console.ReadLine();
+        if (obj.almostPalin(s) == true)
+        {
+            Console.WriteLine($"{s} is an almost Palindrome");
+        }
+        else
+        {
+            Console.WriteLine("Cannot be transformed to a palindrome in exactly one term");
+        }
+
+        Console.WriteLine("Enter a string");
+        string? s1 = Console.ReadLine();
+        Console.WriteLine($"After replacing the characters with their position: {obj.replaceLetter(s1)}");
+    }
+}
---
+++ PracticalPrograms/PracticalTest1Day2/PracticalTest1Day2/Program.cs
@@ -0,0 +1,110 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.DataAnnotations;
+
+class AnagramDuplicate
+{
+    //to check if anagram of a string exists in the other string
+    public bool anagram(string? s1,string? s2)
+    {
+        int i,j;
+        if (s1.Length > s2.Length) return false;
+
+        for(i = 0; i < s2.Length; i++)
+        {
+            if (s1.IndexOf(s2[i]) != -1)
+            {
+                j = i;
+                while (j < s1.Length)
+                {
+                    if (s1.IndexOf(s2[j]) == -1)
+                    {
+                        return false;
+                    }
+                    j++;
+                }
+                return true;
+            }
+        }
+        return true;
+    }
+
+
+    //to count the number of duplicate pairs in a string
+    public int countPairs(string? s)
+    {
+        int i, count = 0;
+        char ch;
+
+        s = sortascii(s);
+        //s = new string(s.OrderBy(c => c).ToArray());//for sorting a string
+        for (i = 0; i < s.Length-1; i++)
+        {
+            ch = s[i+1];
+            if (s[i] == ch)
+            {
+                count++;
+                i += 1;
+            }
+        }
+        return count;
+    }
+
+    public string? sortascii(string? s)
+    {
+        string str = "";
+        char[] ch = new char[s.Length];
+        char t;
+
+        for(int i = 0; i < s.Length; i++)
+        {
+            ch[i] = s[i];
+        }
+        for (int i = 0; i < s.Length-1; i++)
+        {
+            for(int j = 0; j < s.Length - 1 - i; j++)
+            {
+                if (ch[j]>ch[j+1])
+                {
+                    t = ch[j];
+                    ch[j] = ch[j + 1];
+                    ch[j + 1] = t;
+                }
+            }
+        }
+        for(int i = 0; i < s.Length; i++)
+        {
+            str += ch[i];
+        }
+
+        /*str += 12;
+
+        str += (char)(s[0]-0);
+        str += s[0];
+
+        str += (char)s[1] - 0;
+        str += (int)s[1];
+        Console.WriteLine(str);*/
+
+        return str;
+    }
+
+    public static void Main(string[] args)
+    {
+        AnagramDuplicate obj = new AnagramDuplicate();
+        string? s1, s2;
+        Console.WriteLine("Enter string 1 whose anagram is to be found:");
+        s1 = Console.ReadLine();
+        Console.WriteLine("Enter string in which anagram is to be found: ");
+        s2 = Console.ReadLine();
+
+        if (obj.anagram(s1, s2))
+            Console.WriteLine("Anagram exists");
+        else
+            Console.WriteLine("Anagram does not exists");
+
+        string? s;
+        Console.WriteLine("Enter string in which duplicates count has to be found:");
+        s= Console.ReadLine();
+        Console.WriteLine("The no of duplicate pairs are: "+obj.countPairs(s));
+    }
+}
